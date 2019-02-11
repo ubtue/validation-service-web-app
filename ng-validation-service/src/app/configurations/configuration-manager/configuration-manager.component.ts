bccb0fbb0676fd976aeb72a7cb1fda454be050f1
Fabian Hamm <fabian.hamm@uni-tuebingen.de>
2019-02-11 13:41:53 +0100

    Fix config breadcrumb undefined bug

--- ng-validation-service/src/app/configurations/configuration-manager/configuration-manager.component.ts
+++ ng-validation-service/src/app/configurations/configuration-manager/configuration-manager.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ConfigurationsService } from '../configurations.service';
 import { Configuration } from 'src/app/shared/model/configuration.model';
 import { Subscribable, Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-configuration-manager',
@@ -11,13 +12,15 @@ import { Subscribable, Subscription } from 'rxjs';
 export class ConfigurationManagerComponent implements OnInit, OnDestroy {
 
   config: Configuration;
-  constructor(private configService: ConfigurationsService) { }
+  constructor(private configService: ConfigurationsService, private route: ActivatedRoute) { }
   configSubscription: Subscription;
 
   ngOnInit() {
+    this.config = this.route.snapshot.data['configuration'];
     this.configSubscription = this.configService.configLoaded.subscribe(
       (config: Configuration) => {
         this.config = config;
+        console.log('sssss' + config);
       }
     );
   }

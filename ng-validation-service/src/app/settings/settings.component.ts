d09f6af9da536ed563f38c8091a194fd5f982b7b
Fabian Hamm <fabian.hamm@uni-tuebingen.de>
2019-03-07 12:36:35 +0100

    Added basic error messages to settings

--- ng-validation-service/src/app/settings/settings.component.ts
+++ ng-validation-service/src/app/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Data, Router } from '@angular/router';
-import { ApplicationSettings } from '../shared/model/settings.model';
+import { ApplicationSettings, MessageTranslation } from '../shared/model/settings.model';
 import { Util } from '../shared/util';
 import { NgForm } from '@angular/forms';
 import { SettingsService } from './settings.service';
@@ -19,6 +19,7 @@ export class SettingsComponent implements OnInit, CanDeactivateGuard {
 
   settings: ApplicationSettings;
   settingsCopy: ApplicationSettings;
+  showTranslations = false;
 
   @ViewChild('form') form: NgForm;
 
@@ -65,6 +66,7 @@ export class SettingsComponent implements OnInit, CanDeactivateGuard {
   }
 
   onSave() {
+    console.log(this.settingsCopy);
     this.settingsService.updateSettings(this.settingsCopy).subscribe(
       (result) => {
         this.settings = <ApplicationSettings>Util.deepCopy(this.settingsCopy);
@@ -78,11 +80,65 @@ export class SettingsComponent implements OnInit, CanDeactivateGuard {
   }
 
   onCancel() {
-    this.form.reset({
+    let resetValues = {
       parallelTasks: this.settings.parallelTasks,
       threadsPerTask: this.settings.threadsPerTask,
       pageSize: this.settings.pageSize,
-    });
+    };
+
+    // Reset translation messages
+    const messageTranslations: MessageTranslation = this.settings.messageTranslations;
+    for (let key of Object.keys(messageTranslations)) {
+      for (let key2 of Object.keys(messageTranslations[key]) ) {
+        resetValues[key + key2] = messageTranslations[key][key2];
+      }
+    }
+    this.form.reset(resetValues);
   }
+
+  /**
+   * Resolve headings from MessageTranslation var keys.
+   * @param key the key of MessageTranslation member
+   */
+  resolveHeading(key:string) {
+    switch (key) {
+      case 'trailerEncryptTranslations':
+        return 'Encypt keyword found in trailer dictionary';
+        break;
+      case 'encryptTranslations':
+        return 'Encypted PDF document detected';
+        break;
+      case 'fontsTranslations':
+        return 'Non embedded font detected';
+        break;
+      case 'multimediaTranslations':
+        return 'Multimedia annotation detected';
+        break;
+      case 'attachmentsTranslations':
+        return 'File attachments detected';
+        break;
+      case 'filesTranslations':
+        return 'Document contains embedded files';
+        break;
+      case 'pdfATranslations':
+        return 'Document is no valid PDF/A';
+        break;
+      case 'pdfAEncryptedTranslations':
+        return 'Document is no valid PDF/A (Encryption)';
+        break;
+      default:
+        return 'Unknown';
+        break;
+    }
+  }
+
+  // TrackBy functions for translations
+
+  trackByFn(index: any, item: any) {
+    return index;
+ }
+  trackByFn2(index: any, item: any) {
+    return index;
+ }
 
 }
--- ng-validation-service/src/app/shared/model/settings.model.ts
+++ ng-validation-service/src/app/shared/model/settings.model.ts
@@ -5,5 +5,58 @@ export interface ApplicationSettings {
 	id: number;
 	parallelTasks: number;
 	threadsPerTask: number;
-	pageSize: number;
+  pageSize: number;
+  messageTranslations: MessageTranslation;
 }
+
+export interface TrailerEncryptTranslation {
+	de: string;
+	en: string;
+}
+
+export interface EncryptTranslation {
+	de: string;
+	en: string;
+}
+
+export interface FontsTranslation {
+	de: string;
+	en: string;
+}
+
+export interface MultimediaTranslation {
+	de: string;
+	en: string;
+}
+
+export interface AttachmentsTranslation {
+	de: string;
+	en: string;
+}
+
+export interface FilesTranslation {
+	de: string;
+	en: string;
+}
+
+export interface PdfATranslation {
+	de: string;
+	en: string;
+}
+
+export interface PdfAEncryptedTranslation {
+	de: string;
+	en: string;
+}
+
+export interface MessageTranslation {
+  pdfATranslations: PdfATranslation;
+	pdfAEncryptedTranslations: PdfAEncryptedTranslation;
+  encryptTranslations: EncryptTranslation;
+	trailerEncryptTranslations: TrailerEncryptTranslation;
+	fontsTranslations: FontsTranslation;
+	multimediaTranslations: MultimediaTranslation;
+	attachmentsTranslations: AttachmentsTranslation;
+	filesTranslations: FilesTranslation;
+}
+
